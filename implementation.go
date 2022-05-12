1d25ab37da6587a8fb0c669add5743bd476a367b
Yevgen Yakovliev <yyakovliev02@gmail.com>
2022-05-12 18:10:46 +0300

    feat: prefix to infix implementation

--- implementation.go
+++ implementation.go
@@ -1,9 +1,44 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	prefixArray := strings.Split(input, " ")
+
+	stack := make([]string, 0)
+
+	var operatorCount int
+	var operandCount int
+	for _, element := range prefixArray {
+		if element == "+" || element == "-" || element == "*" || element == "/" || element == "^" {
+			operatorCount++
+		} else {
+			operandCount++
+		}
+	}
+
+	if operatorCount != operandCount-1 {
+		return "", errors.New("Invalid prefix equation")
+	}
+
+	for i := len(prefixArray) - 1; i >= 0; i-- {
+		if prefixArray[i] != "+" && prefixArray[i] != "-" && prefixArray[i] != "*" && prefixArray[i] != "/" && prefixArray[i] != "^" {
+			stack = append(stack, prefixArray[i])
+		} else {
+			if len(stack) >= 2 {
+				num1 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				num2 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				stack = append(stack, "("+num1+prefixArray[i]+num2+")")
+			} else {
+				return "", errors.New("Invalid prefix equation")
+			}
+		}
+	}
+
+	return stack[0], nil
 }
