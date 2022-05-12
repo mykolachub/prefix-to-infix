0668ef7b2113938f03d19864d1c827edf4b295c8
Amelin Viacheslav <1.slava.1.amelin.1@gmail.com>
2022-05-12 18:34:00 +0300

    test: added tests for implementation

--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,63 @@
 package lab2
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestSimpleOne(t *testing.T) {
+	res, err := PrefixToInfix("+ 5 * - 4 2 3")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, "(5+((4-2)*3))", res)
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+func TestSimpleTwo(t *testing.T) {
+	res, err := PrefixToInfix("^ + 6 5 + 1 1")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "((6+5)^(1+1))", res)
+	}
+}
 
-	// Output:
-	// 2 2 +
+func TestSimpleThree(t *testing.T) {
+	res, err := PrefixToInfix("- 9 + 5 8")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(9-(5+8))", res)
+	}
+}
+
+func TestSimpleFour(t *testing.T) {
+	res, err := PrefixToInfix("/ 6 ^ 4 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(6/(4^2))", res)
+	}
+}
+
+func TestAdvancedOne(t *testing.T) {
+	res, err := PrefixToInfix("- 1 * + 5 * - 4 2 3 / 6 ^ 4 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(1-((5+((4-2)*3))*(6/(4^2))))", res)
+	}
+}
+
+func TestAdvancedTwo(t *testing.T) {
+	res, err := PrefixToInfix("+ - 4 2 - - 9 + 5 8 ^ + 6 5 + 1 1")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "((4-2)+((9-(5+8))-((6+5)^(1+1))))", res)
+	}
+}
+
+func TestAdvancedThree(t *testing.T) {
+	res, err := PrefixToInfix("+ 5 - 3 ^ 6 + 4 - 5 + 6 - 4 ^ 2 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(5+(3-(6^(4+(5-(6+(4-(2^2))))))))", res)
+	}
+}
+
+func TestAdvancedFour(t *testing.T) {
+	res, err := PrefixToInfix("+ 8 / - - 5 2 3 ^ * / 2 3 6 7")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "(8+(((5-2)-3)/(((2/3)*6)^7)))", res)
+	}
 }
